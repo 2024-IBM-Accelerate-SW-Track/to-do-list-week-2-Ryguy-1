caf9a2116af835f84ebd8fb6506660fd920f3600
Ryland Birchmeier <rylandbirchmeier@gmail.com>
2024-06-12 20:04:35 -0400

    added can't do duplicates

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,6 +14,9 @@ class Home extends Component {
   // the addTodo function simply creates a new array that includes the user submitted todo item and then
   // updates the state with the new list.
   addTodo = (todo) => {
+    if (this.state.todos.find((item) => item.content === todo.content)) {
+      return;
+    }
     // In React, keys or ids in a list help identify which items have changed, been added or removed. Keys
     // should not share duplicate values.
     // To avoid having dup values, we use the Math.random() function to generate a random value for a todo id.
